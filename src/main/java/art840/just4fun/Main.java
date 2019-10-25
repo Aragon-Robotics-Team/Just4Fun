1874ecc59c979e65c4a145f36445513083a2d248
Daniel Z <5725958+dzil123@users.noreply.github.com>
2019-10-24 17:23:52 -0700

    initial commit

---
+++ src/main/java/art840/just4fun/Main.java
@@ -0,0 +1,12 @@
+package art840.just4fun;
+
+import edu.wpi.first.wpilibj.RobotBase;
+
+public final class Main {
+    private Main() {
+    }
+
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+    }
+}
---
+++ src/main/java/art840/just4fun/Robot.java
@@ -0,0 +1,26 @@
+package art840.just4fun;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+
+public class Robot extends TimedRobot {
+    public void robotInit() {
+    }
+
+    public void autonomousInit() {
+    }
+
+    public void autonomousPeriodic() {
+    }
+
+    public void teleopInit() {
+    }
+
+    public void teleopPeriodic() {
+    }
+
+    public void testInit() {
+    }
+
+    public void testPeriodic() {
+    }
+}
