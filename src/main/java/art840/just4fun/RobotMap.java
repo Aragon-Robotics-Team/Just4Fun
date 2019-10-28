55a18bb83e2eaed9bd2988379bc31ab2df3ef161
TomdaBomb2197 <56703250+TomdaBomb2197@users.noreply.github.com>
2019-10-28 16:38:37 -0700

    added talon ports and speed, implemented drivetrain subsytem

---
+++ src/main/java/art840/just4fun/RobotMap.java
@@ -0,0 +1,7 @@
+package art840.just4fun;
+
+public class RobotMap {
+    public static int portDriveTalonLeft = 0;
+    public static int portDriveTalonRight = 0;
+    
+}
--- src/main/java/art840/just4fun/subsystems/Drivetrain.java
+++ src/main/java/art840/just4fun/subsystems/Drivetrain.java
@@ -1,8 +1,23 @@
 package art840.just4fun.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import art840.just4fun.RobotMap;
 
 public class Drivetrain extends Subsystem {
+    TalonSRX talonL = new TalonSRX(RobotMap.portDriveTalonLeft);
+    TalonSRX talonR = new TalonSRX(RobotMap.portDriveTalonRight);
+
+    public void drive(double speedl, double speedr) {
+        talonL.set(ControlMode.PercentOutput, speedl);
+        talonR.set(ControlMode.PercentOutput, speedr);
+    }
+    
+    public void drive(double speedr) {
+        drive(speedr, speedr);
+    }
+
     protected void initDefaultCommand() {
     }
 }
