05a8658c81ca8afa9eb7ea93572882d87d0c1a20
TomdaBomb2197 <56703250+TomdaBomb2197@users.noreply.github.com>
2019-10-28 17:42:17 -0700

    updated ports for f310 controller

--- src/main/java/art840/just4fun/OI.java
+++ src/main/java/art840/just4fun/OI.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Joystick;
 public class OI {
     Joystick joystick = new Joystick(0);
 
+    // logitech f310
     public double getXLeft() {
         return joystick.getRawAxis(0);
     }
@@ -14,10 +15,10 @@ public class OI {
     }
 
     public double getXRight() {
-        return joystick.getRawAxis(0);
+        return joystick.getRawAxis(4);
     }
 
     public double getYRight() {
-        return joystick.getRawAxis(1) * -1;
+        return joystick.getRawAxis(5) * -1;
     }
 }
