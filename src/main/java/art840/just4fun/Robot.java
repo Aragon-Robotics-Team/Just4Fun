c31c2f23cebf559ba87e77786e3da113d11a148a
Daniel Z <5725958+dzil123@users.noreply.github.com>
2019-11-21 21:00:55 -0800

    testing the spark

--- src/main/java/art840/just4fun/Robot.java
+++ src/main/java/art840/just4fun/Robot.java
@@ -3,12 +3,38 @@ package art840.just4fun;
 import art840.just4fun.command.TeleopMode;
 import art840.just4fun.subsystems.Drivetrain;
 import art840.just4fun.util.RobotUtil;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Robot extends RobotUtil {
     public static Drivetrain drivetrain = new Drivetrain();
     public static OI oi = new OI();
 
+    public static CANSparkMax spark;
+    
     public void teleopInit() {
         (new TeleopMode()).start();
+    }
+
+    public void robotInit() {
+        spark = new CANSparkMax(3, MotorType.kBrushless);
+        
+        spark.restoreFactoryDefaults();
+        spark.setMotorType(MotorType.kBrushless);
+        spark.setOpenLoopRampRate(.1);
+    }
+
+    public void testPeriodic() {
+        double x = oi.getYRight();
+        
+        x += oi.buttonB.get() ? 1.0 : 0.0;
+        x += oi.buttonY.get() ? -1.0 : 0.0;
+        
+        x = Math.min(1.0, Math.max(-1.0, x));
+        
+        spark.set(x);
+        
+        // spark.set(oi.getYLeft());
+    
     }
 }
