7282d369fc0abfd4353a079eede14faef54e3856
Daniel Z <5725958+dzil123@users.noreply.github.com>
2019-11-25 10:24:49 -0800

    Test 1

--- src/main/java/art840/just4fun/subsystems/Drivetrain.java
+++ src/main/java/art840/just4fun/subsystems/Drivetrain.java
@@ -2,11 +2,15 @@ package art840.just4fun.subsystems;
 
 import art840.just4fun.RobotMap;
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.Faults;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Drivetrain extends Subsystem {
     TalonSRX talonL = new TalonSRX(RobotMap.portDriveTalonLeft);
@@ -15,7 +19,17 @@ public class Drivetrain extends Subsystem {
     VictorSPX victorL = new VictorSPX(RobotMap.portDriveVictorLeft);
     VictorSPX victorR = new VictorSPX(RobotMap.portDriveVictorRight);
 
+    Faults lFault = new Faults();
+    Faults rFault = new Faults();
+
+    int n = 0;
+
     public Drivetrain() {
+        configController(talonL);
+        configController(talonR);
+        configController(victorL);
+        configController(victorR);
+
         victorL.follow(talonL);
         victorR.follow(talonR);
 
@@ -24,6 +38,9 @@ public class Drivetrain extends Subsystem {
 
         talonR.setInverted(InvertType.InvertMotorOutput);
         victorR.setInverted(InvertType.FollowMaster);
+
+        configEncoder(talonL);
+        configEncoder(talonR);
     }
 
     public void drive(double speedl, double speedr) {
@@ -66,6 +83,41 @@ public class Drivetrain extends Subsystem {
     public void setRamping(double rampingTime) {
         talonR.configOpenloopRamp(rampingTime);
         talonL.configOpenloopRamp(rampingTime);
+    }
+
+    void configController(BaseMotorController talon) {
+        talon.clearStickyFaults();
+        talon.configFactoryDefault();
+    }
+
+    void configEncoder(TalonSRX talon) {
+        talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
+        talon.setSensorPhase(false);
+    }
+
+    public void periodic() {
+        SmartDashboard.putNumber("Left Velocity", talonL.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("Left Position", talonL.getSelectedSensorPosition());
+
+        SmartDashboard.putNumber("Right Velocity", talonR.getSelectedSensorVelocity());
+        SmartDashboard.putNumber("Right Position", talonR.getSelectedSensorPosition());
+
+        n++;
+
+        if (n < 50) {
+            return;
+        }
+
+        n = 0;
+
+        talonL.getFaults(lFault);
+        talonR.getFaults(rFault);
+
+        System.out.println("Left: " + lFault + "\n");
+        System.out.println("Right: " + rFault + "\n");
+
+        SmartDashboard.putBoolean("Left Phase Bad", lFault.SensorOutOfPhase);
+        SmartDashboard.putBoolean("Right Phase Bad", rFault.SensorOutOfPhase);
     }
 
     protected void initDefaultCommand() {}
