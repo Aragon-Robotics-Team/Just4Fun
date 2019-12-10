9c42b469027be9880c623067e024df2979f3b8ac
TomdaBomb2197 <56703250+TomdaBomb2197@users.noreply.github.com>
2019-12-09 16:23:24 -0800

    Got rid of unneeded code

--- src/main/java/art840/just4fun/subsystems/Drivetrain.java
+++ src/main/java/art840/just4fun/subsystems/Drivetrain.java
@@ -3,13 +3,11 @@ package art840.just4fun.subsystems;
 import art840.just4fun.RobotMap;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
-import com.ctre.phoenix.motorcontrol.Faults;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -21,34 +19,12 @@ public class Drivetrain extends Subsystem {
     VictorSPX victorL = new VictorSPX(RobotMap.portDriveVictorLeft);
     VictorSPX victorR = new VictorSPX(RobotMap.portDriveVictorRight);
 
-    Faults lFault = new Faults();
-    Faults rFault = new Faults();
-
-    int n = 0;
-
     public Drivetrain() {
-
-        // var config = new TalonSRXConfiguration();
-
-        // config.slot0.kP = 0.2;
-        // config.slot0.kI = 1.0;
-        // config.slot0.integralZone = 100;
-        // config.voltageCompSaturation = 11;
-        // config.primaryPID.selectedFeedbackSensor = FeedbackDevice.QuadEncoder;
-
-        // enable vcomp
-        // invert
-        // follow
-        // encoder phase
-        // reset encoder positon
-
-        // talonL.configAllSettings(allConfigs)
-
         configController(talonL);
         configController(talonR);
         configController(victorL);
         configController(victorR);
-        
+
         setVoltageCompensation(11);
 
         victorL.follow(talonL);
@@ -79,13 +55,15 @@ public class Drivetrain extends Subsystem {
         speedl *= RobotMap.maxTicks;
         speedr *= RobotMap.maxTicks;
 
-        talonL.set(ControlMode.Velocity, (int)(speedl), DemandType.ArbitraryFeedForward, calc_A_FF(speedl));
-        talonR.set(ControlMode.Velocity, (int)(speedr), DemandType.ArbitraryFeedForward, calc_A_FF(speedr));
+        talonL.set(ControlMode.Velocity, (int) (speedl), DemandType.ArbitraryFeedForward,
+                calc_A_FF(speedl));
+        talonR.set(ControlMode.Velocity, (int) (speedr), DemandType.ArbitraryFeedForward,
+                calc_A_FF(speedr));
     }
-    
+
     double calc_A_FF(double speed) {
         double FF = 0.00;
-        
+
         if (Math.abs(speed) < 2) {
             return 0;
         } else if (speed > 0) {
@@ -127,7 +105,7 @@ public class Drivetrain extends Subsystem {
     public void setRamping(double rampingTime) {
         talonR.configOpenloopRamp(rampingTime);
         talonL.configOpenloopRamp(rampingTime);
-        
+
         talonR.configClosedloopRamp(rampingTime);
         talonL.configClosedloopRamp(rampingTime);
     }
@@ -141,7 +119,7 @@ public class Drivetrain extends Subsystem {
         talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder);
         talon.setSensorPhase(true);
         talon.setSelectedSensorPosition(0);
-        
+
         talon.config_kP(0, .2);
         talon.config_IntegralZone(0, 300);
         talon.config_kI(0, .001);
@@ -163,29 +141,12 @@ public class Drivetrain extends Subsystem {
         SmartDashboard.putNumber("Right Error", talonR.getClosedLoopError());
         SmartDashboard.putNumber("Right Target", talonR.getClosedLoopTarget());
         SmartDashboard.putNumber("Right Output", talonR.getMotorOutputPercent());
-
-        n++;
-
-        if (n < 50) {
-            return;
-        }
-
-        n = 0;
-
-        talonL.getFaults(lFault);
-        talonR.getFaults(rFault);
-
-        // System.out.println("Left: " + lFault + "\n");
-        // System.out.println("Right: " + rFault + "\n");
-
-        // SmartDashboard.putBoolean("Left Phase Bad", lFault.SensorOutOfPhase);
-        // SmartDashboard.putBoolean("Right Phase Bad", rFault.SensorOutOfPhase);
     }
-    
+
     public void setVoltageCompensation(double voltage) {
         talonR.configVoltageCompSaturation(voltage);
         talonR.enableVoltageCompensation(true);
-        
+
         talonL.configVoltageCompSaturation(voltage);
         talonL.enableVoltageCompensation(true);
     }
